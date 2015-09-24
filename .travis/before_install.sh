393e4137b582cf072ddfa3e084125bbfb62b36ed
Thomas <motom001@gmail.com>
2015-09-24 19:55:21 +0200

    force to delete setuptools*.egg

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -4,4 +4,6 @@ set -e # Exit immediately if a command exits with a non-zero status.
 set -x # Print commands and their arguments as they are executed.
 
 pip install --upgrade pip
+pip uninstall -y setuptools
+rm -f /usr/local/lib/python2.7/dist-packages/setuptools-12.0.5-py2.7.egg
 pip install --upgrade setuptools

7682c90b74b9a66e4b7b70f7358745e55adcef9c
Thomas <motom001@gmail.com>
2015-09-24 21:05:06 +0200

    try to test also doorpi as daemon 4

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e # Exit immediately if a command exits with a non-zero status.
+#set -e # Exit immediately if a command exits with a non-zero status.
 set -x # Print commands and their arguments as they are executed.
 
 if [[ $START_MODE = "application" ]]; then
@@ -9,12 +9,17 @@ fi
 
 if [[ $START_MODE = "daemon" ]]; then
     sudo service doorpi status
+    if [ $? -ne 3 ] exit 1
     sleep 5
     sudo service doorpi start
+    if [ $? -ne 0 ] exit 1
     sleep 5
     sudo service doorpi status
+    if [ $? -ne 0 ] exit 1
     sleep 5
     sudo service doorpi stop
+    if [ $? -ne 0 ] exit 1
     sleep 5
     sudo service doorpi status
+    if [ $? -ne 3 ] exit 1
 fi

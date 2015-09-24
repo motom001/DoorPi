eedad1fe13035415a75e0c21d811a581fd78709f
Thomas <motom001@gmail.com>
2015-09-24 20:09:40 +0200

    add pip --version

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -4,6 +4,7 @@ set -e # Exit immediately if a command exits with a non-zero status.
 set -x # Print commands and their arguments as they are executed.
 
 pip install --upgrade pip
+pip --version
 pip uninstall -y setuptools
-sudo rm -f /usr/local/lib/python2.7/dist-packages/setuptools-12.0.5-py2.7.egg
+sudo rm -f /usr/local/lib/python2.7/dist-packages/setuptools*.egg
 pip install --upgrade setuptools

8834ab78190aa84e0db5e083bc81419cef5987df
Thomas <motom001@gmail.com>
2015-09-24 22:15:53 +0200

    installer for app without sudo and for daemon with sudo

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,3 +3,10 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 set -x # Print commands and their arguments as they are executed.
 
+if [[ $START_MODE = "application" ]]; then
+    python setup.py install
+fi
+
+if [[ $START_MODE = "daemon" ]]; then
+    sudo -H python setup.py install
+fi

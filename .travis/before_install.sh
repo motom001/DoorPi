b1222281313eaa07e295e1e36d5ed506b8bb844d
Thomas <motom001@gmail.com>
2015-09-24 19:59:37 +0200

    sudo for remove setuptools*.egg

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -5,5 +5,5 @@ set -x # Print commands and their arguments as they are executed.
 
 pip install --upgrade pip
 pip uninstall -y setuptools
-rm -f /usr/local/lib/python2.7/dist-packages/setuptools-12.0.5-py2.7.egg
+sudo rm -f /usr/local/lib/python2.7/dist-packages/setuptools-12.0.5-py2.7.egg
 pip install --upgrade setuptools

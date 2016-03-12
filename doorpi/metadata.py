c1f178a2c75ea53245fba7743b6890b31d813dde
Thomas <motom001@users.noreply.github.com>
2016-03-12 23:25:18 +0100

    new version number for stable sign

--- doorpi/metadata.py
+++ doorpi/metadata.py
@@ -9,7 +9,7 @@ import os
 package = 'DoorPi'
 project = "VoIP Door-Intercomstation with Raspberry Pi"
 project_no_spaces = project.replace(' ', '')
-version = '2.4.1.9'
+version = '2.5.0.0'
 description = 'provide intercomstation to the doorstation by VoIP'
 keywords = ['intercom', 'VoIP', 'doorstation', 'home automation', 'IoT']
 authors = ['Thomas Meissner']

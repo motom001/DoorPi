25e942b1df55ed5d1c6a20767e0e12024c8e0845
Thomas <motom001@users.noreply.github.com>
2016-05-23 21:21:10 +0200

    add new config key
    ```
    [SIP-Phone]
    factory_config_path=/home/pi/.linphonerc
    
    [linphone]
    sound_noisegate=1
    net_download_bw=0
    ...
    ```

--- doorpi/sipphone/from_linphone.py
+++ doorpi/sipphone/from_linphone.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import logging
+
 logger = logging.getLogger(__name__)
 logger.debug("%s loaded", __name__)
 
@@ -18,6 +18,7 @@ from doorpi.media.CreateDialTone import generate_dial_tone
 
 conf = DoorPi().config
 
+
 def log_handler(level, msg):
     if "pylinphone_Core_instance_method_iterate" in msg: return
     if "pylinphone_Core_get_current_call" in msg: return
@@ -26,29 +27,41 @@ def log_handler(level, msg):
     method = getattr(logger, level)
     method(msg)
 
+
 if logger.getEffectiveLevel() <= 5: lin.set_log_handler(log_handler)
 
+
 def get(*args, **kwargs): return LinPhone(*args, **kwargs)
+
+
 class LinPhone(SipphoneAbstractBaseClass):
+    @property
+    def name(self):
+        return 'linphone wrapper'
 
     @property
-    def name(self): return 'linphone wrapper'
+    def lib(self):
+        return self.__lib
 
     @property
-    def lib(self): return self.__Lib
-    @property
-    def core(self): return self.__Lib
+    def core(self):
+        return self.__lib
 
     @property
-    def recorder(self): return self.__recorder
+    def recorder(self):
+        return self.__recorder
+
     __recorder = None
 
     @property
-    def player(self): return self.__player
+    def player(self):
+        return self.__player
+
     __player = None
 
     @property
-    def current_call(self): return self.core.current_call
+    def current_call(self):
+        return self.core.current_call
 
     @property
     def video_devices(self):
@@ -56,7 +69,7 @@ class LinPhone(SipphoneAbstractBaseClass):
             all_devices = []
             for video_device in self.core.video_devices:
                 all_devices.append({
-                  'name':       video_device
+                    'name': video_device
                 })
             return all_devices
         except Exception:
@@ -68,9 +81,9 @@ class LinPhone(SipphoneAbstractBaseClass):
             all_devices = []
             for sound_device in self.core.sound_devices:
                 all_devices.append({
-                  'name':       sound_device,
-                  'capture':    self.core.sound_device_can_capture(sound_device),
-                  'record':     self.core.sound_device_can_playback(sound_device)
+                    'name': sound_device,
+                    'capture': self.core.sound_device_can_capture(sound_device),
+                    'record': self.core.sound_device_can_playback(sound_device)
                 })
             return all_devices
         except Exception as exp:
@@ -83,10 +96,10 @@ class LinPhone(SipphoneAbstractBaseClass):
             all_codecs = []
             for codec in payloads:
                 all_codecs.append({
-                    'name':         codec.mime_type,
-                    'channels':     codec.channels,
-                    'bitrate':      codec.normal_bitrate,
-                    'enable':       self.core.payload_type_enabled(codec)
+                    'name': codec.mime_type,
+                    'channels': codec.channels,
+                    'bitrate': codec.normal_bitrate,
+                    'enable': self.core.payload_type_enabled(codec)
                 })
             return all_codecs
         except Exception as exp:
@@ -111,17 +124,17 @@ class LinPhone(SipphoneAbstractBaseClass):
     def current_call_dump(self):
         try:
             return {
-                'direction':        'incoming' if self.current_call.dir == 0 else 'outgoing',
-                'remote_uri':       self.current_call.remote_address_as_string,
-                'total_time':       self.current_call_duration,
-                'level_incoming':   self.current_call.record_volume,
-                'level_outgoing':   self.current_call.play_volume,
-                'camera':           self.current_call.camera_enabled
+                'direction': 'incoming' if self.current_call.dir == 0 else 'outgoing',
+                'remote_uri': self.current_call.remote_address_as_string,
+                'total_time': self.current_call_duration,
+                'level_incoming': self.current_call.record_volume,
+                'level_outgoing': self.current_call.play_volume,
+                'camera': self.current_call.camera_enabled
             }
         except Exception:
             return {}
 
-    #TODO: Datetime from linphone CallLog.start_date is more then 30 sec different to python datetime.utcnow()?
+    # TODO: Datetime from linphone CallLog.start_date is more then 30 sec different to python datetime.utcnow()?
     __current_call_start_datetime = datetime.datetime.utcnow()
 
     @property
@@ -136,7 +149,7 @@ class LinPhone(SipphoneAbstractBaseClass):
         logger.debug('reset current call start datetime to %s', self.__current_call_start_datetime)
         return self.__current_call_start_datetime
 
-    def __init__(self, whitelist = list(), *args, **kwargs):
+    def __init__(self, whitelist=list(), *args, **kwargs):
         logger.debug("__init__")
 
         DoorPi().event_handler.register_action('OnShutdown', self.destroy)
@@ -152,46 +165,70 @@ class LinPhone(SipphoneAbstractBaseClass):
         DoorPi().event_handler.register_event('OnSipPhoneMakeCall', __name__)
         DoorPi().event_handler.register_event('OnSipPhoneMakeCallFailed', __name__)
         DoorPi().event_handler.register_event('AfterSipPhoneMakeCall', __name__)
-        
+
         DoorPi().event_handler.register_event('OnSipPhoneCallTimeoutNoResponse', __name__)
         DoorPi().event_handler.register_event('OnSipPhoneCallTimeoutMaxCalltime', __name__)
 
         DoorPi().event_handler.register_event('OnPlayerCreated', __name__)
 
-        #http://pythonhosted.org/linphone/api_reference.html#linphone.Core.new
+        # http://pythonhosted.org/linphone/api_reference.html#linphone.Core.new
         self.callback = LinphoneCallbacks()
         config_path = None
-        factory_config_path = None
-        self.__Lib = lin.Core.new(
-            self.callback.used_callbacks,
+        factory_config_path = conf.get(SIPPHONE_SECTION, 'factory_config_path', '')
+
+        self.__lib_config = lin.LpConfig.new_with_factory(
             config_path,
-            factory_config_path
+            None if factory_config_path is '' else factory_config_path
         )
-        self.core.primary_contact = '%s <sip:doorpi@127.0.0.1>'%conf.get(SIPPHONE_SECTION, "identity", 'DoorPi')
+
+        for config_key in conf.get_keys('linphone', 'rtp_'):
+            self.__lib_config.set_string('rtp', config_key[4:], conf.get('linphone', config_key))
+        for config_key in conf.get_keys('linphone', 'sip_'):
+            self.__lib_config.set_string('rtp', config_key[4:], conf.get('linphone', config_key))
+        for config_key in conf.get_keys('linphone', 'video_'):
+            self.__lib_config.set_string('rtp', config_key[6:], conf.get('linphone', config_key))
+        for config_key in conf.get_keys('linphone', 'net_'):
+            self.__lib_config.set_string('rtp', config_key[4:], conf.get('linphone', config_key))
+        for config_key in conf.get_keys('linphone', 'sound_'):
+            self.__lib_config.set_string('rtp', config_key[6:], conf.get('linphone', config_key))
+
+        self.__lib = lin.Core.new_with_config(
+            self.callback.used_callbacks,
+            self.__lib_config
+        )
+
+        self.core.primary_contact = '%s <sip:doorpi@127.0.0.1>' % conf.get(SIPPHONE_SECTION, "identity", 'DoorPi')
 
     def start(self):
         DoorPi().event_handler('OnSipPhoneCreate', __name__)
         self.core.max_calls = conf.get_int(SIPPHONE_SECTION, 'ua.max_calls', 2)
         self.core.echo_cancellation_enabled = conf.get_bool(SIPPHONE_SECTION, 'echo_cancellation_enabled', False)
-        
+
         # set local listen ports, default: random
-        self.core.sip_transports = lin.SipTransports(conf.get_int(SIPPHONE_SECTION, 'local_port', 5060), conf.get_int(SIPPHONE_SECTION, 'local_port', 5060), -1, -1)
+        self.core.sip_transports = lin.SipTransports(conf.get_int(SIPPHONE_SECTION, 'local_port', 5060),
+                                                     conf.get_int(SIPPHONE_SECTION, 'local_port', 5060), -1, -1)
 
         self.core.video_display_enabled = conf.get_bool(SIPPHONE_SECTION, 'video_display_enabled', False)
         self.core.stun_server = conf.get(SIPPHONE_SECTION, 'stun_server', '')
         firewall_policy = conf.get(SIPPHONE_SECTION, 'FirewallPolicy', 'PolicyNoFirewall')
-        if firewall_policy == "PolicyNoFirewall": self.core.firewall_policy = lin.FirewallPolicy.PolicyNoFirewall
-        elif firewall_policy == "PolicyUseNatAddress": self.core.firewall_policy = lin.FirewallPolicy.PolicyUseNatAddress
-        elif firewall_policy == "PolicyUseStun": self.core.firewall_policy = lin.FirewallPolicy.PolicyUseStun
-        elif firewall_policy == "PolicyUseIce": self.core.firewall_policy = lin.FirewallPolicy.PolicyUseIce
-        elif firewall_policy == "PolicyUseUpnp": self.core.firewall_policy = lin.FirewallPolicy.PolicyUseUpnp
-        else: self.core.firewall_policy = lin.FirewallPolicy.PolicyNoFirewall
+        if firewall_policy == "PolicyNoFirewall":
+            self.core.firewall_policy = lin.FirewallPolicy.PolicyNoFirewall
+        elif firewall_policy == "PolicyUseNatAddress":
+            self.core.firewall_policy = lin.FirewallPolicy.PolicyUseNatAddress
+        elif firewall_policy == "PolicyUseStun":
+            self.core.firewall_policy = lin.FirewallPolicy.PolicyUseStun
+        elif firewall_policy == "PolicyUseIce":
+            self.core.firewall_policy = lin.FirewallPolicy.PolicyUseIce
+        elif firewall_policy == "PolicyUseUpnp":
+            self.core.firewall_policy = lin.FirewallPolicy.PolicyUseUpnp
+        else:
+            self.core.firewall_policy = lin.FirewallPolicy.PolicyNoFirewall
 
-        #http://pythonhosted.org/linphone/api_reference.html#linphone.Core.in_call_timeout
-        #After this timeout period, the call is automatically hangup.
+        # http://pythonhosted.org/linphone/api_reference.html#linphone.Core.in_call_timeout
+        # After this timeout period, the call is automatically hangup.
         self.core.in_call_timeout = conf.get_int(SIPPHONE_SECTION, 'max_call_time', 120)
-        #http://pythonhosted.org/linphone/api_reference.html#linphone.Core.inc_timeout
-        #If an incoming call isn’t answered for this timeout period, it is automatically declined.
+        # http://pythonhosted.org/linphone/api_reference.html#linphone.Core.inc_timeout
+        # If an incoming call isn’t answered for this timeout period, it is automatically declined.
         self.core.inc_timeout = conf.get_int(SIPPHONE_SECTION, 'call_timeout', 15)
 
         self.__player = LinphonePlayer()
@@ -208,10 +245,10 @@ class LinPhone(SipphoneAbstractBaseClass):
             logger.debug("------------------------------------")
             for sound_device in self.core.sound_devices:
                 logger.debug("| %s | %s  | %s",
-                    'X' if self.core.sound_device_can_capture(sound_device) else 'O',
-                    'X' if self.core.sound_device_can_playback(sound_device) else 'O',
-                    sound_device
-                )
+                             'X' if self.core.sound_device_can_capture(sound_device) else 'O',
+                             'X' if self.core.sound_device_can_playback(sound_device) else 'O',
+                             sound_device
+                             )
             logger.debug("------------------------------------")
             logger.debug("using capture_device: %s", self.core.capture_device)
             logger.debug("using playback_device: %s", self.core.playback_device)
@@ -225,7 +262,6 @@ class LinPhone(SipphoneAbstractBaseClass):
             else:
                 logger.debug('disable audio codec %s', codec.mime_type)
                 self.core.enable_payload_type(codec, False)
-
 
         if len(self.core.video_devices) == 0:
             self.core.video_capture_enabled = False
@@ -267,9 +303,9 @@ class LinPhone(SipphoneAbstractBaseClass):
             logger.info('using DoorPi with SIP-Server')
             proxy_cfg = self.core.create_proxy_config()
             proxy_cfg.identity_address = lin.Address.new("%s <sip:%s@%s>" % (
-                    conf.get(SIPPHONE_SECTION, "identity", 'DoorPi'), username, server)
-            )
-            proxy_cfg.server_addr = "sip:%s"%server
+                conf.get(SIPPHONE_SECTION, "identity", 'DoorPi'), username, server)
+                                                         )
+            proxy_cfg.server_addr = "sip:%s" % server
             proxy_cfg.register_enabled = True
             self.core.add_proxy_config(proxy_cfg)
             self.core.default_proxy_config = proxy_cfg
@@ -281,7 +317,7 @@ class LinPhone(SipphoneAbstractBaseClass):
             proxy_cfg.register_enabled = False
             self.core.add_proxy_config(proxy_cfg)
             self.core.default_proxy_config = proxy_cfg
-            logger.debug('%s',self.core.proxy_config_list)
+            logger.debug('%s', self.core.proxy_config_list)
 
         logger.debug("start successfully")
 
@@ -301,18 +337,20 @@ class LinPhone(SipphoneAbstractBaseClass):
 
         if self.current_call.state < lin.CallState.Connected:
             if self.current_call_duration > self.core.inc_timeout - 0.5:
-                logger.info("call timeout - hangup current call after %s seconds (max. %s)", self.current_call_duration, self.core.inc_timeout)
+                logger.info("call timeout - hangup current call after %s seconds (max. %s)", self.current_call_duration,
+                            self.core.inc_timeout)
                 self.core.terminate_all_calls()
                 DoorPi().event_handler('OnSipPhoneCallTimeoutNoResponse', __name__)
         else:
             if int(self.current_call_duration) > self.core.in_call_timeout - 0.5:
-                logger.info("max call time reached - hangup current call after %s seconds (max. %s)", self.current_call_duration, self.core.in_call_timeout)
+                logger.info("max call time reached - hangup current call after %s seconds (max. %s)",
+                            self.current_call_duration, self.core.in_call_timeout)
                 self.core.terminate_all_calls()
                 DoorPi().event_handler('OnSipPhoneCallTimeoutMaxCalltime', __name__)
 
     def call(self, number):
-        DoorPi().event_handler('BeforeSipPhoneMakeCall', __name__, {'number':number})
-        logger.debug("call (%s)",str(number))
+        DoorPi().event_handler('BeforeSipPhoneMakeCall', __name__, {'number': number})
+        logger.debug("call (%s)", str(number))
         if not self.current_call:
             logger.debug('no current call -> start new call')
             self.reset_call_start_datetime()
@@ -320,15 +358,15 @@ class LinPhone(SipphoneAbstractBaseClass):
                 if DoorPi().event_handler.db.get_event_log_entries_count('OnSipPhoneMakeCallFailed') > 5:
                     logger.error('failed to execute call five times')
                 else:
-                    DoorPi().event_handler('OnSipPhoneMakeCallFailed', __name__, {'number':number})
+                    DoorPi().event_handler('OnSipPhoneMakeCallFailed', __name__, {'number': number})
                 return None
-            DoorPi().event_handler('OnSipPhoneMakeCall', __name__, {'number':number})
+            DoorPi().event_handler('OnSipPhoneMakeCall', __name__, {'number': number})
         elif number in self.current_call.remote_address.as_string_uri_only():
             if self.current_call_duration <= 2:
                 logger.debug("same call %s again while call is running since %s seconds? -> skip",
                              self.core.current_call.remote_address.as_string_uri_only(),
                              self.current_call_duration
-                )
+                             )
             else:
                 logger.debug("press twice with call duration > 1 second? Want to hangup current call? OK...")
                 self.core.terminate_all_calls()
@@ -337,7 +375,7 @@ class LinPhone(SipphoneAbstractBaseClass):
             self.core.terminate_all_calls()
             self.call(number)
 
-        DoorPi().event_handler('AfterSipPhoneMakeCall', __name__, {'number':number})
+        DoorPi().event_handler('AfterSipPhoneMakeCall', __name__, {'number': number})
         return self.current_call
 
     def is_admin_number(self, remote_uri):
@@ -349,4 +387,3 @@ class LinPhone(SipphoneAbstractBaseClass):
             self.core.terminate_call(self.current_call)
         else:
             logger.debug("Ignoring hangup request as there is no ongoing call")
-

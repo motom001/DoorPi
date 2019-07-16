7c6e3eb145fc893e47ea1bf6ff326c494a1ecae4
Wüstengecko <1579756+Wuestengecko@users.noreply.github.com>
2019-07-16 20:00:54 +0200

    Bring setup.py up to standard
    - Unify with current coding standard
    - Move data files into `data` subdirectory
    - Install data files relative to python's prefix

--- setup.py
+++ setup.py
@@ -1,37 +1,51 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+#!/usr/bin/python3
 
 import os
+import sys
+
 from doorpi import metadata
 from setuptools import setup, find_packages
-from setuptools.command.install import install as _install
+from setuptools.command.install import install
+
 
 base_path = os.path.dirname(os.path.abspath(__file__))
+etc = "/etc" if sys.prefix == "/usr" else "etc"
+
+
+# Python version check
+if sys.version_info[:2] < (3, 6):
+    print("*** ERROR: Current python version is too old", file=sys.stderr)
+    print("This application requires Python 3.6 or newer", file=sys.stderr)
+    print("Please upgrade your Python installation", file=sys.stderr)
+
 
 def read(filename):
     with open(os.path.join(base_path, filename)) as f:
         file_content = f.read()
     return file_content
 
-# Hook `install' command to process template files (*.in)
-class install(_install):
+
+# Hook install command to process template files (*.in)
+class installhook(install):
     def run(self):
         substkeys={
-            'package': metadata.package.lower(),
-            'project': metadata.project,
-            'prefix': self.prefix,
+            "package": metadata.package.lower(),
+            "project": metadata.project,
+            "prefix": self.prefix,
         }
-        substfiles=[f for f in os.listdir('.') if f.endswith('.in')]
+        substfiles=[f for f in os.listdir("data") if f.endswith(".in")]
         for f in substfiles:
-            with open(os.path.join(base_path, f[:-3]), "w") as outfile, open(os.path.join(base_path, f), "r") as tplfile:
+            with open(os.path.join(base_path, "data", f[:-3]), "w") as outfile, \
+                open(os.path.join(base_path, "data", f), "r") as tplfile:
                 content = tplfile.read()
                 for k, v in substkeys.items():
-                    content = content.replace('!!%s!!'%k, v)
+                    content = content.replace(f"!!{k}!!", v)
                 outfile.write(content)
-        _install.run(self)
+        super().run()
+
 
 setup(
-    cmdclass={'install': install},
+    cmdclass={"install": installhook},
     license=metadata.license,
     name=metadata.package,
     version=metadata.version,
@@ -42,59 +56,54 @@ setup(
     url=metadata.url,
     keywords=metadata.keywords,
     description=metadata.description,
-    long_description=read('README.rst'),
+    long_description=read("README.rst"),
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: System Administrators',
-        'License :: Free for non-commercial use',
-        'Natural Language :: German',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Topic :: Documentation',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: System :: Installation/Setup',
-        'Topic :: System :: Software Distribution',
-        'Topic :: Communications :: Internet Phone',
-        'Topic :: Communications :: Telephony',
-        'Topic :: Multimedia :: Sound/Audio :: Capture/Recording',
-        'Topic :: Multimedia :: Video :: Capture',
-        'Topic :: Multimedia :: Video :: Conversion',
-        'Topic :: Security',
-        'Topic :: System :: Emulators',
-        'Topic :: System :: Filesystems',
-        'Topic :: System :: Hardware',
-        'Topic :: Utilities'
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: Free for non-commercial use",
+        "Natural Language :: German",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Communications :: Internet Phone",
+        "Topic :: Communications :: Telephony",
+        "Topic :: Home Automation",
+        "Topic :: Multimedia :: Sound/Audio :: Capture/Recording",
+        "Topic :: Multimedia :: Video :: Capture",
+        "Topic :: Security",
+        "Topic :: System :: Hardware",
+        "Topic :: Utilities",
     ],
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    install_requires=['requests >= 2.7.0'],
+    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
+    install_requires=["requests >= 2.7.0"],
     extras_require={
-        'gpio': ['RPi.GPIO >= 0.5.11'],
-        'webdoc': ['docutils >= 0.14'],
-        'camera': ['picamera >= 1.10'],
-        'piface': ['pifacecommon >= 4.1.2', 'pifacedigitalio >= 3.0.5'],
-        'rfid': ['pyserial >= 2.7'],
-        'files_pseudokb': ['watchdog >= 0.8.3'],
+        "camera": ["picamera >= 1.10"],
+        "files_pseudokb": ["watchdog >= 0.8.3"],
+        "gpio": ["RPi.GPIO >= 0.5.11"],
+        "piface": ["pifacecommon >= 4.1.2", "pifacedigitalio >= 3.0.5"],
+        "rfid": ["pyserial >= 2.7"],
+        "webdoc": ["docutils >= 0.14"],
     },
     platforms=["any"],
     use_2to3=False,
     zip_safe=False,  # don't use eggs
     entry_points={
-        'console_scripts': [
-            'doorpi_cli = doorpi.main:entry_point'
+        "console_scripts": [
+            "doorpi_cli = doorpi.main:entry_point"
         ]
     },
     data_files=[
         # default config file
-        ('/etc/doorpi', ['doorpi.ini']),
+        (f"{etc}/doorpi", ["data/doorpi.ini"]),
         # init script and systemd service
-        ('/etc/init.d', ['doorpi.sh']),
-        ('/usr/lib/systemd/system', ['doorpi.service', 'doorpi.socket']),
+        (f"{etc}/init.d", ["data/doorpi.sh"]),
+        ("lib/systemd/system", ["data/doorpi.service", "data/doorpi.socket"]),
         # default dialtone
-        ('/usr/share/doorpi', ['data/dialtone.wav']),
+        ("share/doorpi", ["data/dialtone.wav"]),
     ],
 )

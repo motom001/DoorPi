541a8d4eaa54b3a7771976dac3f62743fb0b8414
Wüstengecko <1579756+Wuestengecko@users.noreply.github.com>
2019-09-29 19:07:56 +0200

    requirements: Bring req_config up to date

--- doorpi/status/requirements_lib/req_config.py
+++ doorpi/status/requirements_lib/req_config.py
@@ -2,21 +2,38 @@ REQUIREMENT = dict(
     fulfilled_with_one=True,
     libraries=dict(
         configparser=dict(
-            text_description='Das Config-Modul wird benötigt um alle Einstellungen in einer Datei abspeichern und später wieder laden zu können.',
+            text_description='Das Config-Modul wird benötigt, um alle Einstellungen in einer Datei abspeichern und später wieder laden zu können.',
             text_installation='Eine Installation ist nicht nötig, da es sich hierbei um ein Python-Standard-Modul handelt.',
             auto_install=False,
             text_test='Der Status kann gestestet werden, indem im Python-Interpreter ``import configparser`` eingeben wird.',
-            text_configuration='''Eine Konfiguration als Eintrag in der Konfigurationsdatei hat logischerweise keinen Sinn.
-Deshalb kann die zu nutzende Config-Datei als Parameter (--configfile) beim DoorPi Start mitgegeben werden. Beispiel::
+            text_configuration=\
+'''Eine Konfiguration als Eintrag in der Konfigurationsdatei hat
+logischerweise keinen Sinn.  Deshalb kann die zu nutzende Config-Datei
+als Parameter (``--configfile``) beim DoorPi Start mitgegeben werden.
+
+Beispiel::
 
     doorpi_cli --configfile /etc/doorpi/doorpi.ini
 
-Wenn der Parameter wegelassen wird, sucht der ConfigParser automatisch nach folgenden Dateien (wobei !BASEPATH! das aktuelle Verzeichnis ist)
+Wird der Parameter nicht angegeben, wird die Datei von einem
+Standardpfad geladen.  Dieser Pfad hängt davon ab, wo DoorPi
+installiert wurde:
 
-1. !BASEPATH!/conf/doorpi.ini
-2. !BASEPATH!/conf/doorpi.cfg
+- Wurde es systemweit unterhalb von ``/usr`` installiert (bspw. mittels
+  AUR-Paket), liegt die Standarddatei in::
 
-Sollte keine Datei vorhanden sein, so wird DoorPi mit Standardwerten gestartet und die Konfigurationsdatei an der ersten möglichen Stelle abgespeichert.
+    /etc/doorpi/doorpi.ini
+- Wurde DoorPi systemweit unterhalb von ``/usr/local`` installiert,
+  liegt die Standarddatei in::
+
+    /usr/local/etc/doorpi/doorpi.ini
+- Wurde das Programm in einem Python Virtualenv installiert, liegt die
+  Datei innerhalb des Virtualenvs::
+
+    $VIRTUAL_ENV/etc/doorpi/doorpi.ini
+
+Sollte die Datei nicht existieren, wird DoorPi mit seinen eingebauten
+Standardwerten gestartet.
 ''',
             text_links={
                 'docs.python.org': 'https://docs.python.org/2.7/library/configparser.html'

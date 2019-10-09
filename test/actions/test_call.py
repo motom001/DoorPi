17532dcde39db754173b3330deef093a327d9add
Wüstengecko <1579756+Wuestengecko@users.noreply.github.com>
2019-10-09 23:04:49 +0200

    Add tests for call-action

---
+++ test/actions/test_call.py
@@ -0,0 +1,20 @@
+from . import EVENT_ID, EVENT_EXTRA
+from ..mocks import DoorPi, DoorPiTestCase
+from unittest.mock import patch
+
+import doorpi
+import doorpi.actions.call as action
+
+
+SIPURL = "sip:null@null"
+
+
+class TestActionCall(DoorPiTestCase):
+
+    @patch('doorpi.DoorPi', DoorPi)
+    def test_action(self):
+        ac = action.instantiate(SIPURL)
+        doorpi.DoorPi().sipphone.call.assert_not_called()
+
+        ac(EVENT_ID, EVENT_EXTRA)
+        doorpi.DoorPi().sipphone.call.assert_called_once_with(SIPURL)

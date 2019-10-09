18169285a687925203aa2adbb27b2123c5476495
Wüstengecko <1579756+Wuestengecko@users.noreply.github.com>
2019-10-09 23:04:49 +0200

    Add tests for os_execute-action

---
+++ test/actions/test_os_execute.py
@@ -0,0 +1,33 @@
+from . import EVENT_ID, EVENT_EXTRA
+from ..mocks import DoorPi, DoorPiTestCase
+from unittest.mock import patch
+
+import doorpi
+import doorpi.actions.os_execute as action
+
+
+# A command that is successful (i.e. returns 0)
+TEST_CMD_SUCCESS = "/bin/true"
+# A command that fails (i.e. returns non-zero)
+TEST_CMD_FAIL = "/bin/false"
+
+
+class TestOSExecuteAction(DoorPiTestCase):
+
+    @patch('doorpi.DoorPi', DoorPi)
+    def _do_test(self, cmd, result):
+        # Simulate real config handling by splitting the command at commas
+        ac = action.instantiate(*cmd.split(","))
+
+        with self.assertLogs("doorpi.actions.os_execute", "INFO") as cm:
+            ac(EVENT_ID, EVENT_EXTRA)
+
+        self.assertEqual(len(cm.records), 2)
+        self.assertEqual(cm.records[0].args, (EVENT_ID, cmd))
+        self.assertEqual(cm.records[1].args, (EVENT_ID, result) if result != 0 else (EVENT_ID,))
+
+    def test_successful(self):
+        self._do_test(TEST_CMD_SUCCESS, 0)
+
+    def test_failing(self):
+        self._do_test(TEST_CMD_FAIL, 1)

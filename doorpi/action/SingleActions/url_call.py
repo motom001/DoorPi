f850a7e366f7481958799636ae61b5f5bb7de734
deviloper-lab <60013459+deviloper-lab@users.noreply.github.com>
2020-02-03 20:14:55 +0100

    - Fixed URL Query for unauthorized access

--- doorpi/action/SingleActions/url_call.py
+++ doorpi/action/SingleActions/url_call.py
@@ -22,20 +22,22 @@ def fire_command(url):
             password = nurl.password
             url = url.replace(username + ':' + password + '@', '')
             url = url.replace(" ", "%20")
-            logger.debug('url: %s' % url)
+            
             ssl._create_default_https_context = ssl._create_unverified_context
             p = urllib2.HTTPPasswordMgrWithDefaultRealm()
             p.add_password(None, url, username, password)
             handler = urllib2.HTTPBasicAuthHandler(p)
             opener = urllib2.build_opener(handler)
             urllib2.install_opener(opener)
+        else:
             url = url.replace(" ", "%20")
-            logger.info('url: %s' % url)
-            return urllib2.urlopen(
-                url=url,
-                data=None,
-                timeout=1
-            )
+            
+        logger.debug('url: %s' % url)
+        return urllib2.urlopen(
+            url=url,
+            data=None,
+            timeout=1
+        )
     except urllib2.HTTPError as exp:
         logger.error('HTTPError: %s - %s' % (exp.code, exp.reason))
     except urllib2.URLError as exp:

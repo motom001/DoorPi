2ac72d5a7b069f0883216bf9e0b3ee86f04712b9
deviloper-lab <60013459+deviloper-lab@users.noreply.github.com>
2020-02-04 12:23:40 +0100

    - PEP8 conformed

--- doorpi/action/SingleActions/url_call.py
+++ doorpi/action/SingleActions/url_call.py
@@ -1,11 +1,4 @@
 # -*- coding: utf-8 -*-
-# thx to pula @ DoorPi forum
-# https://www.doorpi.org/forum/thread/25-http-request/?postID=596#post596
-
-import logging
-logger = logging.getLogger(__name__)
-logger.debug("%s loaded", __name__)
-
 from doorpi.action.base import SingleAction
 import doorpi
 
@@ -13,16 +6,20 @@ import urllib2
 import ssl
 import urlparse
 
+import logging
+logger = logging.getLogger(__name__)
+logger.debug("%s loaded", __name__)
+
 
 def fire_command(url):
     try:
-        if "@" in url:
+        if '@' in url:
             nurl = urlparse.urlsplit(url)
             username = nurl.username
             password = nurl.password
             url = url.replace(username + ':' + password + '@', '')
-            url = url.replace(" ", "%20")
-            
+            url = url.replace(' ', '%20')
+
             ssl._create_default_https_context = ssl._create_unverified_context
             p = urllib2.HTTPPasswordMgrWithDefaultRealm()
             p.add_password(None, url, username, password)
@@ -31,13 +28,12 @@ def fire_command(url):
             urllib2.install_opener(opener)
         else:
             url = url.replace(" ", "%20")
-            
+
         logger.debug('url: %s' % url)
         return urllib2.urlopen(
             url=url,
             data=None,
-            timeout=1
-        )
+            timeout=1)
     except urllib2.HTTPError as exp:
         logger.error('HTTPError: %s - %s' % (exp.code, exp.reason))
     except urllib2.URLError as exp:

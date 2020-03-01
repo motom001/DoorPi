0daf4727b80f5356b3cd3dd6ff8807325e578fb4
deviloper-lab <60013459+deviloper-lab@users.noreply.github.com>
2020-03-01 22:01:57 +0100

    remove shebang

--- doorpi/action/SingleActions/statusfile.py
+++ doorpi/action/SingleActions/statusfile.py
@@ -1,13 +1,14 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import doorpi
+from doorpi.action.base import SingleAction
+from doorpi.status.status_class import DoorPiStatus
+
+from io import open
 
 import logging
 logger = logging.getLogger(__name__)
-logger.debug("%s loaded", __name__)
+logger.debug('%s loaded', __name__)
 
-from doorpi.action.base import SingleAction
-import doorpi
-from doorpi.status.status_class import DoorPiStatus
 
 def write_statusfile(filename, filecontent):
     try:
@@ -23,32 +24,28 @@ def write_statusfile(filename, filecontent):
             filecontent = filecontent.replace('!DOORPI_STATUS.json!', doorpi_status_json)
 
         except:
-            logger.exception("error while creating status")
+            logger.exception('error while creating status')
 
     except:
-        logger.warning("while action statusfile - error to get DoorPi().parse_string")
+        logger.warning('while action statusfile - error to get DoorPi().parse_string')
         return False
 
     try:
-        file = open(filename, 'w')
-        try:
+        with open(filename, 'w') as file
             file.write(filecontent)
             file.flush()
-        finally:
-            file.close()
     except IOError as e:
-        logger.warning("while action statusfile - I/O error(%s): %s"%(e.errno, e.strerror))
+        logger.warning(('while action statusfile - I/O error({0}): {1}').format(e.errno, e.strerror))
         return False
-
     return True
 
 def get(parameters):
     parameter_list = parameters.split(',')
-    if len(parameter_list) < 2: return None
+    if len(parameter_list) < 2:
+        return None
 
     filename = parameter_list[0]
     filecontent = ''.join(parameter_list[1:])
-
     return SleepAction(write_statusfile, filename, filecontent)
 
 class SleepAction(SingleAction):

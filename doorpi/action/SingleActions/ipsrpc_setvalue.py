e6a6b628439f7309708efe58d272091e9a747760
deviloper-lab <60013459+deviloper-lab@users.noreply.github.com>
2020-03-01 21:24:53 +0100

    remove shebang

--- doorpi/action/SingleActions/ipsrpc_setvalue.py
+++ doorpi/action/SingleActions/ipsrpc_setvalue.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import doorpi
+from doorpi.action.base import SingleAction
+
+import requests
+from requests.auth import HTTPBasicAuth
+import json
 
 import logging
 logger = logging.getLogger(__name__)
-logger.debug("%s loaded", __name__)
+logger.debug('%s loaded', __name__)
 
-import doorpi
-import requests
-import json
-from requests.auth import HTTPBasicAuth
-from doorpi.action.base import SingleAction
 
 def ips_rpc_create_config():
     config = {}
@@ -22,57 +22,64 @@ def ips_rpc_create_config():
 
 def ips_rpc_fire(method, config, *parameters):
     payload = {
-       "method": method,
-       "params": parameters,
-       "jsonrpc": config['jsonrpc'],
-       "id": 0,
-    }
+       'method': method,
+       'params': parameters,
+       'jsonrpc': config['jsonrpc'],
+       'id': 0, }
     return requests.post(
         config['webservice_url'],
-        headers = config['headers'],
-        auth = HTTPBasicAuth(config['username'], config['password']),
-        data = json.dumps(payload)
+        headers=config['headers'],
+        auth=HTTPBasicAuth(config['username'], config['password']),
+        data=json.dumps(payload)
     )
 
-def ips_rpc_check_variable_exists(key, config = None):
-    if config is None: config = ips_rpc_create_config()
+def ips_rpc_check_variable_exists(key, config=None):
+    if config is None:
+        config = ips_rpc_create_config()
+
     response = ips_rpc_fire('IPS_VariableExists', config, key)
     return response.json['result']
 
-def ips_rpc_get_variable_type(key, config = None):
-    if config is None: config = ips_rpc_create_config()
+def ips_rpc_get_variable_type(key, config=None):
+    if config is None:
+        config = ips_rpc_create_config()
+
     response = ips_rpc_fire('IPS_GetVariable', config, key)
     return response.json['result']['VariableValue']['ValueType']
 
-def ips_rpc_set_value(key, value, config = None):
+def ips_rpc_set_value(key, value, config=None):
     try:
-        if config is None: config = ips_rpc_create_config()
-        if ips_rpc_check_variable_exists(key, config) is not True: raise Exception("var %s doesn't exist", key)
+        if config is None:
+            config = ips_rpc_create_config()
+        if ips_rpc_check_variable_exists(key, config) is not True:
+            raise Exception('no variable %s found', key)
         type = ips_rpc_get_variable_type(key, config)
-        if type is None: raise Exception("type of var %s couldn't find", key)
-        # http://www.ip-symcon.de/service/dokumentation/befehlsreferenz/variablenverwaltung/ips-getvariable/
-        # Variablentyp (0: Boolean, 1: Integer, 2: Float, 3: String)
+        if type is None:
+            raise Exception('type of variable %s unknown', key)
+        # variable type (0: boolean, 1: integer, 2: float, 3: string)
         elif type == 0:
-            if value.lower() in ['true', 'yes', '1']: value = True
-            else: value = False
-        elif type == 1: value = int(value)
-        elif type == 2: value = float(value)
-        elif type == 3: value = str(value)
-        else: value = str(value)
+            value = (value.lower() in ['true', 'yes', '1'])
+        elif type == 1:
+            value = int(value)
+        elif type == 2:
+            value = float(value)
+        else:
+            value = str(value)
         ips_rpc_fire('SetValue', config, key, value)
     except Exception as ex:
-        logger.exception("couldn't send IpsRpc (%s)", ex)
+        logger.exception('send IpsRpc (%s) failed', ex)
         return False
     return True
 
 def get(parameters):
     parameter_list = parameters.split(',')
-    if len(parameter_list) is not 2: return None
+    if len(parameter_list) is not 2:
+        return None
 
     key = int(parameter_list[0])
     value = parameter_list[1]
-
     return IpsRpcSetValueAction(ips_rpc_set_value, key, value)
+
 
 class IpsRpcSetValueAction(SingleAction):
     pass

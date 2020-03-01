78a51da754725e044a846993f7e4ab9842df7ffc
deviloper-lab <60013459+deviloper-lab@users.noreply.github.com>
2020-03-01 21:12:33 +0100

    remove shebang

--- doorpi/action/SingleActions/hangup.py
+++ doorpi/action/SingleActions/hangup.py
@@ -1,25 +1,25 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-import logging
-logger = logging.getLogger(__name__)
-logger.debug("%s loaded", __name__)
-
 import doorpi
 from doorpi.action.base import SingleAction
 
 from time import sleep
 
+import logging
+logger = logging.getLogger(__name__)
+logger.debug('%s loaded', __name__)
+
+
 def hangup(waittime):
-    logger.trace("hangup requested")
     if waittime > 0:
-        logger.debug("Waiting %s seconds before sending hangup request", waittime)
+        logger.debug('Waiting %s seconds before sending hangup request', waittime)
         sleep(float(waittime))
     return doorpi.DoorPi().sipphone.hangup()
 
 def get(parameters):
-    if not parameters.isdigit(): return None
+    if not parameters.isdigit(): 
+        return None
     return HangupAction(hangup, parameters)
+
 
 class HangupAction(SingleAction):
     pass

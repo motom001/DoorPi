6c36d780e682f53a076c6a493133136c1d50021e
deviloper-lab <60013459+deviloper-lab@users.noreply.github.com>
2020-03-01 22:22:58 +0100

    remove shebang

--- doorpi/action/base.py
+++ doorpi/action/base.py
@@ -1,12 +1,11 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from time import sleep
+import importlib
 
 import logging
 logger = logging.getLogger(__name__)
-logger.debug("%s loaded", __name__)
+logger.debug('%s loaded', __name__)
 
-from time import sleep
-import importlib
 
 class SingleAction:
     action_name = None
@@ -14,11 +13,10 @@ class SingleAction:
 
     @property
     def name(self):
-        return "%s with args %s and kwargs %s" % (
-            self.action_name,
-            self.__args,
-            self.__kwargs
-        )
+        return ('{0} with args {1} and kwargs {2}').format(
+            self.action_name, 
+            self.__args, 
+            self.__kwargs)
 
     def __init__(self, callback, *args, **kwargs):
         self.__callback = callback
@@ -32,25 +30,20 @@ class SingleAction:
     def __str__(self):
         return self.name
 
-    def run(self, silent_mode = False):
+    def run(self, silent_mode=False):
         if not silent_mode:
-            logger.trace('run %s with args %s and kwargs %s',
+            logger.trace(('run {0} with args {1} and kwargs {2}').format(
                          self.__class__.__name__,
                          self.__args,
-                         self.__kwargs
-            )
+                         self.__kwargs))
         try:
             if len(self.__args) is not 0 and len(self.__kwargs) is not 0:
-                #print "args and kwargs"
                 return self.__callback(*self.__args, **self.__kwargs)
             elif len(self.__args) is 0 and len(self.__kwargs) is not 0:
-                #print "no args but kwargs"
                 return self.__callback(**self.__kwargs)
             elif len(self.__args) is not 0 and len(self.__kwargs) is 0:
-                #print "args and no kwargs"
                 return self.__callback(*self.__args)
             else:
-                #print "no args and no kwargs"
                 return self.__callback()
         except TypeError as ex:
             logger.exception(ex)
@@ -59,12 +52,14 @@ class SingleAction:
     def from_string(config_string):
         try:
             action_name = config_string.split(':', 1)[0]
-            if not action_name: return None
-            try: parameters = config_string.split(':', 1)[1]
-            except: parameters = ""
-            return importlib.import_module('doorpi.action.SingleActions.'+action_name).get(
-                parameters
-            )
+            if not action_name:
+                return None
+            try:
+                parameters = config_string.split(':', 1)[1]
+            except:
+                parameters = ''
+            return importlib.import_module('doorpi.action.SingleActions.' + action_name).get(
+                parameters)
         except:
-            logger.exception('error while creating SingleAction from config string: %s',config_string)
+            logger.exception('error while creating SingleAction from config string: %s', config_string)
             return None

cafd002e3c85b3cd23393ed495b181cf9b630fb4
deviloper-lab <60013459+deviloper-lab@users.noreply.github.com>
2020-03-02 00:00:17 +0100

    clean code

--- doorpi/action/SingleActions/statusfile.py
+++ doorpi/action/SingleActions/statusfile.py
@@ -10,22 +10,20 @@ logger = logging.getLogger(__name__)
 logger.debug('%s loaded', __name__)
 
 
-def write_statusfile(filename, filecontent):
+def write_statusfile(filename, content):
     try:
         filename = doorpi.DoorPi().parse_string(filename)
-        filecontent = doorpi.DoorPi().parse_string(filecontent)
+        content = doorpi.DoorPi().parse_string(content.strip() + '\n')
 
         try:
             doorpi_status = DoorPiStatus(doorpi.DoorPi())
             doorpi_status_json_beautified = doorpi_status.json_beautified
             doorpi_status_json = doorpi_status.json
 
-            filecontent = filecontent.replace('!DOORPI_STATUS.json_beautified!', doorpi_status_json_beautified)
-            filecontent = filecontent.replace('!DOORPI_STATUS.json!', doorpi_status_json)
-
+            content = content.replace('!DOORPI_STATUS.json_beautified!', doorpi_status_json_beautified)
+            content = content.replace('!DOORPI_STATUS.json!', doorpi_status_json)
         except:
             logger.exception('error while creating status')
-
     except:
         logger.warning('while action statusfile - error to get DoorPi().parse_string')
         return False
@@ -33,7 +31,6 @@ def write_statusfile(filename, filecontent):
     try:
         with open(filename, 'w') as file
             file.write(filecontent)
-            file.flush()
     except IOError as e:
         logger.warning(('while action statusfile - I/O error({0}): {1}').format(e.errno, e.strerror))
         return False
@@ -45,8 +42,8 @@ def get(parameters):
         return None
 
     filename = parameter_list[0]
-    filecontent = ''.join(parameter_list[1:])
-    return SleepAction(write_statusfile, filename, filecontent)
+    content = ''.join(parameter_list[1:])
+    return StatusFileAction(write_statusfile, filename, content)
 
-class SleepAction(SingleAction):
+class StatusFileAction(SingleAction):
     pass

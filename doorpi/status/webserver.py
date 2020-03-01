dc2418c6ad5d66b6920113173e331123959aa505
deviloper-lab <60013459+deviloper-lab@users.noreply.github.com>
2020-03-01 13:33:17 +0100

    Update webserver.py

--- doorpi/status/webserver.py
+++ doorpi/status/webserver.py
@@ -1,28 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import doorpi
+from doorpi.action.base import SingleAction
+from doorpi.status.webserver_lib.session_handler import SessionHandler
+from doorpi.status.webserver_lib.request_handler import DoorPiWebRequestHandler
+
+from http.server import HTTPServer
+from socketserver import ThreadingMixIn
+from random import randrange
 
 import logging
 logger = logging.getLogger(__name__)
 logger.debug("%s loaded", __name__)
 
-from BaseHTTPServer import HTTPServer
-from SocketServer import ThreadingMixIn
 
-from random import randrange
+class WebServerStartupAction(SingleAction):
+    pass
 
-import doorpi
-from doorpi.action.base import SingleAction
 
-from doorpi.status.webserver_lib.session_handler import SessionHandler
-from doorpi.status.webserver_lib.request_handler import DoorPiWebRequestHandler
+class WebServerFakeRequestAction(SingleAction):
+    pass
 
-class WebServerStartupAction(SingleAction): pass
-class WebServerFakeRequestAction(SingleAction): pass
-class WebServerShutdownAction(SingleAction): pass
-class WebServerInformUrl(SingleAction): pass
+
+class WebServerShutdownAction(SingleAction):
+    pass
+
+
+class WebServerInformUrl(SingleAction):
+    pass
+
 
 DOORPIWEB_SECTION = 'DoorPiWeb'
 CONF_AREA_PREFIX = 'AREA_'
+
 
 def load_webserver():
     ip = doorpi.DoorPi().config.get(DOORPIWEB_SECTION, 'ip', '')
@@ -45,6 +55,7 @@ def load_webserver():
 
     return doorpiweb_object
 
+
 def check_config(config):
     errors = []
     warnings = []
@@ -62,43 +73,48 @@ def check_config(config):
         errors.append("no WritePermission found")
 
     for group in groups_with_write_permissions:
-        if group not in groups: warnings.append("group %s doesn't exist but is assigned to WritePermission" % group)
+        if group not in groups:
+            warnings.append(('group {} does not exist but is assigned to WritePermission').format(group))
 
     if len(groups_with_read_permissions) == 0:
-        warnings.append("no ReadPermission found")
+        warnings.append('no ReadPermission found')
 
     for group in groups_with_read_permissions:
-        if group not in groups: warnings.append("group %s doesn't exist but is assigned to ReadPermission" % group)
+        if group not in groups:
+            warnings.append(('group {} does not exist but is assigned to ReadPermission').format(group))
 
     for group in groups:
         users_in_group = config.get_list('Group', group)
         for user_in_group in users_in_group:
             if user_in_group not in users:
-                warnings.append("user %s is assigned to group %s but doesn't exist as user" % (user_in_group, group))
+                warnings.append(('user {0} is assigned to group {1} but does not exist as user').format(user_in_group, group))
 
     config_section = config.get_sections()
 
     for group in groups_with_write_permissions:
         modules = config.get_list('WritePermission', group)
         for module in modules:
-            if CONF_AREA_PREFIX+module not in config_section:
-                warnings.append("module %s doesn't exist but is assigned to group %s in WritePermission" % (module, group))
+            if (CONF_AREA_PREFIX + module) not in config_section:
+                warnings.append(('module {0} does not exist but is assigned to group {1} in WritePermission').format(module, group))
 
     for group in groups_with_read_permissions:
         modules = config.get_list('ReadPermission', group)
         for module in modules:
             if CONF_AREA_PREFIX+module not in config_section:
-                warnings.append("module %s doesn't exist but is assigned to group %s in ReadPermission" % (module, group))
+                warnings.append(('module {0} does not exist but is assigned to group {1} in ReadPermission').format(module, group))
 
-    for info in infos: logger.info(info)
-    for warning in warnings: logger.error(warning)
-    for error in errors: logger.error(error)
+    for info in infos:
+        logger.info(info)
+    for warning in warnings:
+        logger.error(warning)
+    for error in errors:
+        logger.error(error)
 
     return {'infos': infos, 'warnings': warnings, 'errors': errors}
 
+
 class DoorPiWeb(ThreadingMixIn, HTTPServer):
     keep_running = True
-
     www = None
     indexfile = None
     loginfile = None
@@ -112,9 +128,9 @@ class DoorPiWeb(ThreadingMixIn, HTTPServer):
     @property
     def own_url(self):
         if self.server_port is 80:
-            return "http://%s/"%self.server_name
+            return ('http://{}/').format(self.server_name)
         else:
-            return "http://%s:%s/"%(self.server_name, self.server_port)
+            return ('http://{0}:{1}/').format(self.server_name, self.server_port)
 
     def inform_own_url(self): logger.info('DoorPiWeb URL is %s', self.own_url)
 
@@ -124,6 +140,7 @@ class DoorPiWeb(ThreadingMixIn, HTTPServer):
             logger.debug('no session handler - creating it now')
             self._session_handler = SessionHandler()
         return self._session_handler
+
     _session_handler = None
 
     @property
@@ -137,7 +154,6 @@ class DoorPiWeb(ThreadingMixIn, HTTPServer):
         self.indexfile = doorpi.DoorPi().config.get_string_parsed(DOORPIWEB_SECTION, 'indexfile', 'index.html')
         self.loginfile = doorpi.DoorPi().config.get_string_parsed(DOORPIWEB_SECTION, 'loginfile', 'login.html')
         self.area_public_name = doorpi.DoorPi().config.get_string_parsed(DOORPIWEB_SECTION, 'public', 'AREA_public')
-        # https://raw.githubusercontent.com/motom001/DoorPiWeb/master/ or http://motom001.github.io/DoorPiWeb/
         self.online_fallback = doorpi.DoorPi().config.get_string_parsed(DOORPIWEB_SECTION, 'online_fallback', 'http://motom001.github.io/DoorPiWeb')
         check_config(self.config)
 
@@ -149,18 +165,21 @@ class DoorPiWeb(ThreadingMixIn, HTTPServer):
         return self
 
     def handle_while_not_shutdown(self):
-        while self.keep_running: self.handle_request()
+        while self.keep_running:
+            self.handle_request()
 
     def fake_request(self):
         try:
-            from urllib2 import urlopen as fake_request
-            fake_request("http://%s:%s/"%(self.server_name, self.server_port), timeout = 0)
-        except: pass
+            from urllib.request import urlopen as fake_request
+            fake_request(('http://{0}:{1}/').format(self.server_name, self.server_port), timeout=0)
+        except:
+            pass
 
     def init_shutdown(self):
         doorpi.DoorPi().event_handler('OnWebServerStop', __name__)
         self.keep_running = False
-        if self.sessions: self.sessions.destroy()
+        if self.sessions:
+            self.sessions.destroy()
         DoorPiWebRequestHandler.destroy()
         self.fake_request()
         doorpi.DoorPi().event_handler.unregister_source(__name__, True)

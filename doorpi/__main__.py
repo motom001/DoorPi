b75b9f3a45f4c2f0387975089f6afdaa418b41ed
Wüstengecko <1579756+Wuestengecko@users.noreply.github.com>
2021-02-05 17:06:34 +0100

    main: Fix "module is not callable" error after shutting down

--- doorpi/__main__.py
+++ doorpi/__main__.py
@@ -109,8 +109,8 @@ def parse_arguments() -> argparse.Namespace:
     return arg_parser.parse_args(args=sys.argv[1:])
 
 
-def entry_point() -> None:
-    """Zero-argument entry point for use with setuptools/distribute."""
+def main() -> None:
+    """Run DoorPi as standalone service"""
     args = parse_arguments()
     init_logger(args)
     LOGGER.info(metadata.epilog)
@@ -134,4 +134,5 @@ def entry_point() -> None:
         instance.destroy()
 
 
-entry_point()
+if __name__ == "__main__":
+    main()
